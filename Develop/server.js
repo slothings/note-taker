25b06dc5678b439f74f639d6066cb67c0988c222
Slothing <seth.r.martineau@gmail.com>
2020-10-07 05:32:12 -0600

    added paths to notes and index

--- Develop/server.js
+++ Develop/server.js
@@ -6,6 +6,17 @@ const fs = require("fs");
 // server setup
 const app = express();
 const PORT = process.env.PORT || 3000; 
+const mainDir = path.join(__dirname, "/public");
+
+// path to notes.html
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(mainDir, "notes.html"));
+});
+
+// path to index.html
+app.get("*", function(req, res) {
+    res.sendFile(path.join(mainDir, "index.html"));
+});
 
 app.listen(PORT, function() {
     console.log(`PORT:${PORT} is online!`);
